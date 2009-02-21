7fe80d43f7aa8b9782b8a461602ae0960a0dcaf2
Jeff Balogh <me@jeffbalogh.org>
2009-02-20 17:08:41 -0800

    adding settings

---
+++ bosley/settings.py
@@ -0,0 +1,12 @@
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+path = lambda x: os.path.join(ROOT, x)
+
+DATABASE = 'sqlite:///' + path('db.sqlite')
+
+REPO = os.path.expanduser('~/public_html/bot/amo')
+
+BASE = 'http://bot.khan.mozilla.org/amo/site/en-US/firefox/tests/xml/?'
+
+TEMPLATE_PATH = path('templates')

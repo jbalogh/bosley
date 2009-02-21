917667ed347d73ee57008b9ac8c2c40c4b667dea
Jeff Balogh <me@jeffbalogh.org>
2009-02-20 17:09:13 -0800

    adding remote

---
+++ bosley/remote.py
@@ -0,0 +1,35 @@
+from pyquery import PyQuery
+from lxml.etree import XMLSyntaxError
+
+import settings
+
+
+class BrokenTest(Exception):
+    pass
+
+
+def query(url):
+    # Force XML parser so we're not forgiving on errors.  Exceptions induce
+    # invalid output, so we want those caught and shown as an error.
+    return PyQuery(url=settings.BASE + url, parser='xml')
+
+
+def discover():
+    return query('discover=1')
+
+
+def cases():
+    d = discover()
+    return [e.text for e in d('case')]
+
+
+def test(case):
+    return query('case=' + case)
+
+
+def analyze(case):
+    try:
+        d = test(case)
+    except XMLSyntaxError:
+        raise BrokenTest
+    return d('pass').size(), d('fail').size()

69f03f6662c30c968a27c6c7d86e0ab9ebaed9e3
Jeff Balogh <me@jeffbalogh.org>
2009-02-23 17:20:18 -0800

    adding the testrunner and version control shim

---
+++ bosley/runtests.py
@@ -0,0 +1,105 @@
+import logging
+from Queue import Queue
+from threading import Thread
+
+import sqlalchemy.orm
+
+import remote, vcs
+from models import Revision, Case, Result
+from utils import get_session, metadata
+
+session = get_session(wsgi=False)
+
+log = logging.getLogger(__file__)
+
+
+def test_commit(id='HEAD'):
+    revdata = vcs.info(id)
+    q = session.query(Revision)
+
+    if q.filter_by(git_id=revdata['git_id']).count() == 0:
+        revision = Revision(**revdata)
+        session.add(revision)
+        session.commit()
+        # The object can't be shared across threads.
+        test_revision(revision.id)
+
+
+class ThreadedTester(Thread):
+
+    def __init__(self, queue, rev):
+        # Sessions can't be shared across threads.
+        self.session = get_session(wsgi=False)
+        self.queue =queue
+        self.rev = rev
+        super(Thread, self).__init__()
+
+    def run(self):
+        while not self.queue.empty():
+            case = self.queue.get()
+            log.debug('%s: Testing %s...' % (self.getName(), case))
+
+            result = self.test(case)
+            result.revision_id = self.rev
+            self.session.add(result)
+            self.session.commit()
+
+            log.debug('%s: Finished %s' % (self.getName(), result))
+
+    def test(self, case_name):
+        try:
+            case = self.session.query(Case).filter_by(name=case_name).one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            case = Case(name=case_name)
+
+        try:
+            passes, fails = remote.analyze(case_name)
+            return Result(case=case, passes=passes, fails=fails)
+        except remote.BrokenTest:
+            return Result(case=case, broken=True)
+
+
+def test_revision(rev):
+    queue = Queue()
+    for case in remote.cases():
+        queue.put(case)
+
+    # XXX: testing speed is currently limited by languageConfig.test.php,
+    # which takes 30s to timeout.  The number of threads could be upped
+    # once this is fixed.
+    num_threads = 3
+    threads = []
+    for i in range(num_threads):
+        t = ThreadedTester(queue, rev)
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        log.debug("Joining " + t.getName())
+        t.join()
+
+
+def populate():
+    metadata.create_all(session.bind)
+
+    oldest = session.query(Revision).order_by('date').first()
+    if oldest is None:
+        commit = 'HEAD'
+    else:
+        commit = vcs.before(oldest.git_id).id
+
+    # Process tests as far back as we can.  Eventually, something will fail.
+    while True:
+        log.debug('Processing %s' % commit)
+        vcs.checkout(commit)
+        try:
+            vcs.apply_testing_patch()
+            test_commit(commit)
+        finally:
+            vcs.reset('%s^' % commit)
+        log.debug('Finished %s' % commit)
+        commit = vcs.before(commit).id
+
+
+if __name__ == '__main__':
+    populate()
---
+++ bosley/vcs.py
@@ -0,0 +1,71 @@
+import logging
+import subprocess
+import sys
+from datetime import datetime
+
+import git
+
+import settings
+
+log = logging.getLogger(__file__)
+repo = git.Repo(settings.REPO)
+
+
+class CommandError(Exception):
+    pass
+
+
+def info(head):
+    c = repo.commits(head)[0]
+    return {'message': c.message,
+            'author': '%s <%s>' % (c.author.name, c.author.email),
+            'date': datetime(*c.committed_date[:7]),
+            'git_id': c.id,
+            'svn_id': svn_id(head, c.id),
+            }
+
+
+def svn_id(head, hash):
+    # gross.
+    cmd = 'git checkout -q %s && git svn info' % head
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,
+                         cwd=settings.REPO)
+    out = p.communicate()[0].split()
+    # Looks like "Revision: xxx", so grab the item after Revision.
+    return out[1 + out.index('Revision:')]
+
+
+def before(id):
+    return repo.commits(id)[0].parents[0]
+
+
+def call(cmd):
+    status = subprocess.call(cmd, cwd=settings.REPO, shell=True)
+    log.debug('call: %s: %s' % (status, cmd))
+    if status != 0:
+        raise CommandError(status)
+
+
+def checkout(id):
+    try:
+        call('git checkout -q %s' % id)
+    except CommandError, status:
+        # The repo is in an unknown state, don't try to do anything.
+        log.error('Bailing after failed checkout!')
+        sys.exit(status)
+
+
+def apply_testing_patch():
+    try:
+        call('git am %s' % settings.path('data/testing.patch'))
+    except CommandError, status:
+        log.error('Failed to apply testing patch!')
+        sys.exit(status)
+
+
+def reset(id):
+    try:
+        call('git reset --hard %s' % id)
+    except CommandError, status:
+        log.error('Resetting failed!')
+        sys.exit(status)

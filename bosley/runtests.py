fcaa24b748ed2a2417375440ef0fd8097b01edfd
Jeff Balogh <me@jeffbalogh.org>
2009-02-25 11:29:05 -0800

    refactoring

--- bosley/runtests.py
+++ bosley/runtests.py
@@ -13,16 +13,38 @@ session = get_session(wsgi=False)
 log = logging.getLogger(__file__)
 
 
-def test_commit(id='HEAD'):
+def test_commit(id):
     revdata = vcs.info(id)
     q = session.query(Revision)
 
-    if q.filter_by(git_id=revdata['git_id']).count() == 0:
-        revision = Revision(**revdata)
-        session.add(revision)
-        session.commit()
-        # The object can't be shared across threads.
-        test_revision(revision.id)
+    if q.filter_by(git_id=revdata['git_id']).count() != 0:
+        return
+
+    revision = Revision(**revdata)
+    session.add(revision)
+    session.commit()
+    # The object can't be shared across threads.
+    test_revision(revision.id)
+
+
+def test_revision(rev):
+    queue = Queue()
+    for case in remote.cases():
+        queue.put(case)
+
+    # XXX: testing speed is currently limited by languageConfig.test.php,
+    # which takes 30s to timeout.  The number of threads could be upped
+    # once this is fixed.
+    num_threads = 3
+    threads = []
+    for i in range(num_threads):
+        t = ThreadedTester(queue, rev)
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        log.debug("Joining " + t.getName())
+        t.join()
 
 
 class ThreadedTester(Thread):
@@ -57,26 +79,6 @@ class ThreadedTester(Thread):
             return Result(case=case, passes=passes, fails=fails)
         except remote.BrokenTest:
             return Result(case=case, broken=True)
-
-
-def test_revision(rev):
-    queue = Queue()
-    for case in remote.cases():
-        queue.put(case)
-
-    # XXX: testing speed is currently limited by languageConfig.test.php,
-    # which takes 30s to timeout.  The number of threads could be upped
-    # once this is fixed.
-    num_threads = 3
-    threads = []
-    for i in range(num_threads):
-        t = ThreadedTester(queue, rev)
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        log.debug("Joining " + t.getName())
-        t.join()
 
 
 def populate():

8a65ac321ec1733fa1ae5f7e5163ed48248ae136
Jeff Balogh <me@jeffbalogh.org>
2009-02-27 22:34:30 -0800

    pulling the stats generator out into a separate function

--- bosley/views.py
+++ bosley/views.py
@@ -11,17 +11,21 @@ def revision_list(request):
     revstats = {}
     revisions = session.query(Revision).order_by(desc(Revision.date))[:25]
 
-    for rev in revisions:
-        results = session.query(Result).filter_by(revision=rev)
-
-        broken = results.filter_by(broken=True).count()
-        failing = results.filter(Result.fails > 0).count()
-
-        passes = results.value(func.sum(Result.passes))
-        fails = results.value(func.sum(Result.fails))
-
-        revstats[rev] = {'broken': broken, 'failing': failing, 'fails': fails,
-                         'passes': passes, 'total': passes + fails}
+    for revision in revisions:
+        revstats[revision] = stats(session, revision)
 
     return render_template('revision_list.html', revisions=revisions,
                            revstats=revstats)
+
+
+def stats(session, revision):
+    results = session.query(Result).filter_by(revision=revision)
+
+    broken = results.filter_by(broken=True).count()
+    failing = results.filter(Result.fails > 0).count()
+
+    passes = results.value(func.sum(Result.passes))
+    fails = results.value(func.sum(Result.fails))
+
+    return {'broken': broken, 'failing': failing, 'fails': fails,
+            'passes': passes, 'total': passes + fails}

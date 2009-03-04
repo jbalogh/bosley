64066f0d8a5e27d9026077cd8dd4d1b4919b3e03
Jeff Balogh <me@jeffbalogh.org>
2009-03-04 09:47:15 -0800

    better setup and teardown

--- bosley/tests/__init__.py
+++ bosley/tests/__init__.py
@@ -1,3 +1,16 @@
-from bosley import settings
+import os
 
-settings.DATABASE = 'sqlite:///:memory:'
+from bosley import settings, utils
+from bosley.tests import fixtures
+
+
+def setup_module():
+    settings.DATABASE += '.test'
+    utils.Session.bind = fixtures.BaseCase.fixture.engine = utils.engine()
+    utils.metadata.create_all(utils.Session.bind)
+
+
+def teardown_module():
+    # XXX: only on sqlite
+    os.remove(settings.DATABASE.replace('sqlite:///', ''))
+    settings.DATABASE.replace('.test', '')

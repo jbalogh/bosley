a116aff7c431d92426bac45309f314660200474c
Jeff Balogh <me@jeffbalogh.org>
2009-04-05 00:18:28 -0700

    STAB

--- bosley/log.py
+++ bosley/log.py
@@ -10,12 +10,15 @@ def stab_java():
     if not os.path.exists(dir):
         os.makedirs(dir)
 
+    level = logging.DEBUG
+
     # Roll over every 3 days, only keep 3 backups.
     h = handlers.TimedRotatingFileHandler(os.path.join(dir, 'bosley.log'),
                                           when='D', interval=3, backupCount=3)
-    h.setLevel(logging.DEBUG)
+    h.setLevel(level)
 
     f = logging.Formatter('%(asctime)s %(levelname)s:%(name)s:%(message)s')
     h.setFormatter(f)
 
     logging.root.addHandler(h)
+    logging.root.setLevel(level)

cff5bd1b947ca4d29e0d0c52d555967f2b14d743
Jeff Balogh <me@jeffbalogh.org>
2009-05-25 01:26:00 -0700

    cache decorator

---
+++ bosley/cache.py
@@ -0,0 +1,23 @@
+from werkzeug.contrib.cache import SimpleCache
+
+
+cache = SimpleCache()
+
+
+def get_cache_key(o):
+    if hasattr(o, 'cache_key'):
+        return o.cache_key
+    else:
+        return str(o)
+
+
+def cached(f):
+    def inner(*args, **kwargs):
+        keys = map(get_cache_key, args)
+        for item in kwargs.items():
+            keys.append('%s:%s' % item)
+        key = f.__name__ + '-'.join(keys)
+        if cache.get(key) is None:
+            cache.set(key, f(*args, **kwargs))
+        return cache.get(key)
+    return inner

1fdba7177cbba7c0fca9931e417e034bac3ae54e
Jeff Balogh <me@jeffbalogh.org>
2009-07-10 10:54:32 -0700

    fixing perlsub and bugzilla filters

--- bosley/filters.py
+++ bosley/filters.py
@@ -6,20 +6,20 @@ from utils import add_to, jinja_env
 
 @add_to(jinja_env.filters)
 def bugzilla(text):
-    return perlsub(text, 'bug (\d+)', settings.BUGZILLA_BUG % '$1')
+    link = '<a href="%s">$0</a>' % (settings.BUGZILLA_BUG % '$1')
+    return perlsub(text, 'bug (\d+)', link)
 
 
 @add_to(jinja_env.filters)
 def perlsub(string, regex, replacement):
     """Does a regex sub; the replacement string can have $n groups."""
     def sub(match):
-        groups = match.groups()
         ret = []
         for s in re.split('(\$\d+)', replacement):
-            match = re.match('\$(\d+)', s)
-            if match:
-                index = int(match.groups()[0]) - 1
-                ret.append(groups[index])
+            m = re.match('\$(\d+)', s)
+            if m:
+                index = int(m.groups()[0])
+                ret.append(match.group(index))
             else:
                 ret.append(s)
         return ''.join(ret)
